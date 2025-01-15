fc6ec1245d527ac967fecdf4fad22e3dfac24f0d
hkaspersen <hakon.kaspersen@protonmail.com>
2025-01-15 11:21:32 +0100

    updated the am_groups table to fit into the update_analyte_sens_group function

--- data-raw/am_groups.R
+++ data-raw/am_groups.R
@@ -21,7 +21,6 @@ con <- dbConnect(
 # Get group data for substances
 am_groups <- as_tibble(tbl(con, "analytt_sens_group")) %>%
   mutate_all(~str_squish(.)) %>%
-  select(-analyttkode_sens) %>%
   filter(!is.na(substans))
 
 usethis::use_data(am_groups, overwrite = TRUE)
